ea33971e61f86ac60de3b98c44ec58948e6b983a
Cruzz-name <lupangch007@gmail.com>
2024-08-21 11:53:22 +0700

    Add files via upload

---
+++ labexam1/receivelogin.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="mainstyle.css">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="button.css">
+    <link rel="stylesheet" href="login.css">
+    <meta charset="UTF-8">
+    <title>Home page</title>
+</head>
+<body>
+    <nav>
+        <div>
+            <h1>บริการจัดหางาน</h1>
+            <ul class="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">ข้อมูลส่วนตัว</a></li>
+                <li><a href="#">ข้อมูลสถานประกอบการ</a></li>
+                <li><a href="#">ประวัติการส่งใบสมัคร</a></li>
+            </ul>
+        </div>
+    </nav>
+    <form action="main.html" method="post" style="text-align: right;">
+        <input class="GoTologin" type="submit" id="OK" name="OK" value="ออกจากระบบ">
+    </form>
+    <div>
+        <a href="profile.html"><img src="Iconmix.png" style="width: 220px; height: 250px;" ></a>
+        
+        <a href="#"><img src="Iconmix.png" style="width: 220px; height: 250px;" ></a>
+        
+        <a href="#"><img src="Iconmix.png" style="width: 220px; height: 250px;" ></a>
+  
+    </div>
+    <div class="text1">ข้อมูลส่วนตัว</div>
+    <div class="text2">ข้อมูลสถานประกอบการ</div>
+    <div class="text3">ประวัติการส่งใบสมัคร</div>
+    <nav class="underbar">
+        <div>
+            <p>บริการจัดหางาน คณะวิทยาศาสตร์และเทคโนโลยี สาขาวิทยาการคอมพิวเตอร์ มหาวิทยาลัยเทคโนโลยีราชมงคลธัญบุรี</p>
+            <p>แจ้งปัญหาการใช้งานหรือต้องการติดต่อเจ้าหน้าที่</p>
+            <p>โทร 081-234-5678</p>
+        </div>
+    </nav>
+</body>
+</html>
---
+++ labexam1/receivesingin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="mainstyle.css">
+    <link rel="stylesheet" href="button.css">
+    <meta charset="UTF-8">
+    <title>Singin</title>
+</head>
+<body>
+    <div>
+        <a href="singin.html"><img src="ผู้สมัครงาน.png" style="width: 300px; height: 200px;" ></a>
+        <a href="#"><img src="สถานประกอบการ.png" style="width: 300px; height: 200px;" ></a>
+    </div>
+</body>
+</html>
