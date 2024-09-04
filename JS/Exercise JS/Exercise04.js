2c8c1833057461e96a74540a62255f7665385903
Cruzz-name <lupangch007@gmail.com>
2024-09-04 11:44:08 +0700

    Update Exercise04.js

--- JS/Exercise JS/Exercise04.js
+++ JS/Exercise JS/Exercise04.js
@@ -1,11 +1,11 @@
 /* เขียนตามสไลด์อาจารย์ */
-const random_hex_color_code = () =>{
+const random_hex_color_code = () => {
     let n = (Math.random()*0xfffff *1000000).toString(16)
     return '#'+n.slice(0,6);
 }
 console.log(random_hex_color_code())
 
 /* แบบเขียนจบในบรรทัดเดียว */
-const random_hex_color_code_Name = ()=> '#'+((Math.random() *0xfffff *1000000).toString(16)).slice(0,6);
+const random_hex_color_code_Name = () => '#'+((Math.random() *0xfffff *1000000).toString(16)).slice(0,6);
 
 console.log(random_hex_color_code_Name())
